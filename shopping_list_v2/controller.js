108b89d9945c3b13d93a375a4394318a2ed88aad
ramibr01 <ramibr01@luther.edu>
2018-10-01 17:29:15 -0500

    Update controller.js

--- shopping_list_v2/controller.js
+++ shopping_list_v2/controller.js
@@ -1,1 +1,47 @@
+var stores = ['Fareway', 'Ace Hardware', 'Caseys', 'The Hatchery', 'Amundsens']
+var sections = ['Produce', 'Meats', 'Cereal', 'Canned Goods', 'Frozen Foods', 'Dairy', 'Liquor', 'Tools', 'Clothing']
+
+var shoppingModel = new ShoppingList()
+var myView = new ShoppingView(shoppingModel)
+
+function clickedon() {
+    let rowcolids = ['itemname', 'qty', 'store', 'section', 'price', 'priority']
+    let vals = {}
+    for (let cid of rowcolids) {
+        vals[cid] = document.getElementById(cid).value;
+    }
+    
+    if (vals.itemname != "" && Number(vals.price)) {
+        let it = new Item(vals.itemname, vals.qty, vals.priority, vals.store, vals.section, vals.price)
+        shoppingModel.addItem(it)
+    }
+}
+
+function populateSelect(selectId, sList) {
+    let sel = document.getElementById(selectId, sList)
+    for (let s of sList) {
+        let opt = document.createElement("option")
+        opt.value = s
+        opt.innerHTML = s
+        sel.appendChild(opt)
+    }
+}
+
+function saveList() {
+    shoppingModel.saveList()
+}
+
+function clearList() {
+    shoppingModel.clearList()
+}
+
+function removePurchased() {
+    shoppingModel.removePurchased()
+}
+
+$(document).ready(function () {
+    populateSelect('store', stores)
+    populateSelect('section', sections)
+    myView.redrawList(shoppingModel)
+})
 
